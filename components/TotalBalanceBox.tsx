09cf2b57b492e44e27356eec5cb39d6db1c54360
KwanJunEr <jonastheitguy@gmail.com>
2024-11-22 15:13:54 +0800

    completed the TotalBalance Card

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { formatAmount } from '@/lib/utils'
 import CountUp from 'react-countup';
 import AnimatedCounter from './AnimatedCounter';
-
+import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/*DoughnutChart*/}
+            {/* DoughnutChart */}
+            <DoughnutChart accounts={accounts}/>
         </div>
         <div className='flex flex-col gap-6'>
             <h2 className='header-2'>
@@ -18,12 +19,11 @@ const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: Totla
                 <p className='total-balance-label'>
                     Total Current Balance 
                 </p>
-                <p className='total-balance-amount flex-center gap-2'>
-                    <AnimatedCounter amount = {totalCurrentBalance}/>
-                  
-                </p>
+                <div className='total-balance-amount flex-center gap-2'>
+                    <AnimatedCounter amount={totalCurrentBalance}/>
+                </div>
             </div>
-         </div>
+        </div>
     </section>
   )
 }
